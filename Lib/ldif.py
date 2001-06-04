bc179d1aee0f0df31bf743cbd0d4babdc1a9755e
leonard <leonard>
2001-06-04 03:05:26 +0000

    add RCS tag fix bug found by Roger Moore <rwmoore@fnal.gov> where objectclass attributes weren't always removed

--- Lib/ldif.py
+++ Lib/ldif.py
@@ -1,3 +1,6 @@
+# python
+# $Id$
+
 """
 ldif.py - Various routines for handling LDIF data
 
@@ -81,6 +84,9 @@ def CreateLDIF(
   attrs = entry.keys()[:]
   try:
     attrs.remove('objectclass')
+  except ValueError:
+    pass
+  try:
     attrs.remove('objectClass')
   except ValueError:
     pass
