bb7caae5c59b55ffa1e3b2a2bd9c57282edc0541
stroeder <stroeder>
2001-12-21 14:22:35 +0000

    New Python wrapper module ldap.ldapobject around LDAPObject implemented by Modules/LDAPObject.c. Removed the obsolete module ldapthreadlock.

--- Lib/ldap/__init__.py
+++ Lib/ldap/__init__.py
@@ -2,3 +2,9 @@
 from _ldap import __version__
 from _ldap import *
 
+from ldapobject import open
+from ldapobject import init
+from ldapobject import initialize
+from ldapobject import set_option
+from ldapobject import get_option
+
