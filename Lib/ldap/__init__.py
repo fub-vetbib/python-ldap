3ff49a61a389af3a4d8685cc6aa39839baad830c
stroeder <stroeder>
2001-12-27 10:59:08 +0000

    Wrapper modules similar structured like the C sources under Modules/.

--- Lib/ldap/__init__.py
+++ Lib/ldap/__init__.py
@@ -1,10 +1,44 @@
 # $Id$
+
 from _ldap import __version__
+
 from _ldap import *
 
-from ldapobject import open
-from ldapobject import init
-from ldapobject import initialize
-from ldapobject import set_option
-from ldapobject import get_option
 
+if __debug__:
+  # Tracing is only supported in debugging mode
+  import sys,traceback
+  _module_debug_level = 0
+
+
+try:
+
+  # Check if Python installation was build with thread support
+  import threading
+
+except ImportError:
+
+  def _ldap_call(func,*args,**kwargs):
+    """
+    Wrapper function if threading module is not available
+    """
+    return apply(func,args,kwargs)
+
+else:
+
+  # Global lock for serializing all calls into underlying LDAP lib
+  _ldap_lock = threading.Lock()
+
+  def _ldap_call(func,*args,**kwargs):
+    """
+    Wrapper function which locks calls to func with via _ldap_lock
+    """
+    _ldap_lock.acquire()
+    try:
+      result = apply(func,args,kwargs)
+    finally:
+      _ldap_lock.release()
+    return result
+
+
+from functions import *
