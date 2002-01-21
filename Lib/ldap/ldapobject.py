d0acae443618aab39519bca789930b3cb69ffec6
stroeder <stroeder>
2002-01-21 00:08:27 +0000

    Renamed parameter for LDAPObject.manage_dsa_it()

--- Lib/ldap/ldapobject.py
+++ Lib/ldap/ldapobject.py
@@ -221,12 +221,12 @@ class LDAPObject:
     """
     self._ldap_call(self._l.flush_cache)
 
-  def manage_dsa_it(self,flag):
+  def manage_dsa_it(self,enable):
     """
     manage_dsa_it() -> None
-    Turn on manageDSAit mode on or off (see draft-zeilenga-ldap-namedref)
+    Enable or disable manageDSAit mode (see draft-zeilenga-ldap-namedref)
     """
-    self._ldap_call(self._l.manage_dsa_it,flag)
+    self._ldap_call(self._l.manage_dsa_it,enable)
   
   def modify(self,dn,modlist):
     """
