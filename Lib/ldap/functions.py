3bef7d52bd97beb564c867a1160da6ccef0ded88
stroeder <stroeder>
2002-01-30 22:04:06 +0000

    open() to be compliant to old docs, init() is the very same

--- Lib/ldap/functions.py
+++ Lib/ldap/functions.py
@@ -34,7 +34,7 @@ from ldap import _ldap_call
 from ldap.ldapobject import LDAPObject
 
 
-def open(host,trace_level=0,trace_file=sys.stdout):
+def open(host,port=389,trace_level=0,trace_file=sys.stdout):
   """
   Return LDAPObject instance by opening LDAP connection to
   specified LDAP host
@@ -50,24 +50,7 @@ def open(host,trace_level=0,trace_file=sys.stdout):
   """
   return LDAPObject('ldap://%s' % (host),trace_level,trace_file)
 
-
-def init(host,port=389,trace_level=0,trace_file=sys.stdout):
-  """
-  Return LDAPObject instance by opening LDAP connection to
-  specified LDAP host
-  
-  Parameters:
-  host
-        LDAP host name, e.g. localhost
-  port
-        integer specifying the port number to use, e.g. 389
-  trace_level
-        If non-zero a trace output of LDAP calls is generated.
-  trace_file
-        File object where to write the trace output to.
-        Default is to use stdout.
-  """
-  return LDAPObject('ldap://%s:%d' % (host,port),trace_level,trace_file)
+init = open
 
 
 def initialize(uri,trace_level=0,trace_file=sys.stdout):
