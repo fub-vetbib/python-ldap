37ab238fb29eef91bff1e1a7f9886310067653e7
stroeder <stroeder>
2002-04-16 08:03:05 +0000

    Patches(addition for schema support contributed by Hans Aschauer

---
+++ Modules/schema.h
@@ -0,0 +1,19 @@
+/* Hans Aschauer <Hans.Aschauer@epost.de>, 2002. Public domain. */
+
+/* 
+ * 
+ * $Id$
+ */
+
+#ifndef __h_schema_
+#define __h_schema_
+
+
+
+#include "Python.h"
+#include "common.h"
+extern void LDAPinit_schema( PyObject* );
+
+
+#endif /* __h_schema_ */
+
