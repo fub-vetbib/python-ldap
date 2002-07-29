3c5adc79e7006767f747372d8d1c670fa451420a
stroeder <stroeder>
2002-07-29 21:10:47 +0000

    Added strlist_minus(), strlist_intersection() and strlist_union()

--- Lib/ldap/cidict.py
+++ Lib/ldap/cidict.py
@@ -26,6 +26,50 @@ class cidict(UserDict):
 	def has_key(self, name):
 		return UserDict.has_key(self, lower(name))
 
+
+def strlist_minus(a,b):
+  """
+  Return list of all items in a which are not in b (a - b).
+  a,b are supposed to be lists of case-insensitive strings.
+  """
+  temp = cidict()
+  for elt in b:
+    temp[elt] = elt
+  result = [
+    elt
+    for elt in a
+    if not temp.has_key(elt)
+  ]
+  return result
+
+
+def strlist_intersection(a,b):
+  """
+  Return intersection of two lists of case-insensitive strings a,b.
+  """
+  temp = cidict()
+  for elt in a:
+    temp[elt] = elt
+  result = [
+    temp[elt]
+    for elt in b
+    if temp.has_key(elt)
+  ]
+  return result
+
+
+def strlist_union(a,b):
+  """
+  Return union of two lists of case-insensitive strings a,b.
+  """
+  temp = cidict()
+  for elt in a:
+    temp[elt] = elt
+  for elt in b:
+    temp[elt] = elt
+  return temp.values()
+
+
 if __name__ == '__main__':
 	x = { 'AbCDeF' : 123 }
 	cix = cidict(x)
