3b543517a2d76b70b5c76aa7f3206b0c99c56150
stroeder <stroeder>
2003-04-11 12:51:01 +0000

    Updated __version__

--- Lib/ldap/schema/__init__.py
+++ Lib/ldap/schema/__init__.py
@@ -7,7 +7,7 @@ See http://python-ldap.sourceforge.net for details.
 \$Id$
 """
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 from ldap.schema.subentry import SubSchema,SCHEMA_ATTRS,SCHEMA_CLASS_MAPPING,SCHEMA_ATTR_MAPPING,urlfetch
 from ldap.schema.models import *
