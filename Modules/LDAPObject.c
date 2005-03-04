0d5b187be24197e9972bc7cab604edd3eebeea30
deepak_giri <deepak_giri>
2005-03-04 17:59:19 +0000

    Reverted the previous edit; doing a run-time version check doesn't buy us anything over an #if statement, and it's more complicated.

--- Modules/LDAPObject.c
+++ Modules/LDAPObject.c
@@ -8,7 +8,6 @@
 #include "Python.h"
 
 #include <math.h>
-#include <stdlib.h>
 #include <limits.h>
 #include "common.h"
 #include "errors.h"
@@ -633,12 +632,6 @@ l_ldap_sasl_interactive_bind_s( LDAPObject* self, PyObject* args )
 {
     char *c_mechanism;
     char *who, *cred;
-    char *parse_format;
-
-    /* According to the docs, the minor version number of Python is always
-     * the 3rd character in the version string. */
-    const char *py_version = Py_GetVersion();
-    int minor_version = atoi(&py_version[2]);
 
     PyObject *serverctrls = Py_None;
     PyObject *clientctrls = Py_None;
@@ -652,19 +645,7 @@ l_ldap_sasl_interactive_bind_s( LDAPObject* self, PyObject* args )
     void *defaults;
     static unsigned sasl_flags = LDAP_SASL_QUIET;
 
-    /* 
-     * In Python 2.3+, a "I" format argument indicates that we're either
-     * converting the Python object into a long or an unsigned int. In versions
-     * prior to that, it will always convert to a long. Since the sasl_flags
-     * variable is an unsigned int, we need to use the "I" flag if we're
-     * running Python 2.3+ and a "i" otherwise. 
-     */
-    if (minor_version < 3)
-        parse_format = "sOOOi";
-    else
-        parse_format = "sOOOI";
-
-    if (!PyArg_ParseTuple(args, parse_format, &who, &SASLObject, &serverctrls, &clientctrls, &sasl_flags ))
+    if (!PyArg_ParseTuple(args, "sOOOI", &who, &SASLObject, &serverctrls, &clientctrls, &sasl_flags ))
       return NULL;
 
     if (not_valid(self)) return NULL;
