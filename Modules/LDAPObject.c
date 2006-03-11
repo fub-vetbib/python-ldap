df19032d58386884967300eb1764945b55c300ef
stroeder <stroeder>
2006-03-11 21:05:57 +0000

    Fixed [ 1440177 ] too few arguments for Py_BuildValue()

--- Modules/LDAPObject.c
+++ Modules/LDAPObject.c
@@ -925,7 +925,7 @@ l_ldap_result3( LDAPObject* self, PyObject *args )
     if (res_type == 0) {
 	/* Polls return (None, None, None, None); timeouts raise an exception */
 	if (timeout == 0)
-		return Py_BuildValue("(OOOO)", Py_None, Py_None, Py_None);
+		return Py_BuildValue("(OOOO)", Py_None, Py_None, Py_None, Py_None);
 	else
 		return LDAPerr(LDAP_TIMEOUT);
     }
