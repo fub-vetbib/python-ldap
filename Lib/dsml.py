57ff4b2db744549620ca8ef754f8e12e9bf795ef
stroeder <stroeder>
2011-07-21 17:53:22 +0000

    Set release version 2.4.2

--- Lib/dsml.py
+++ Lib/dsml.py
@@ -10,7 +10,7 @@ Python compability note:
 Tested with Python 2.0+.
 """
 
-__version__ = '2.4.1'
+__version__ = '2.4.2'
 
 import string,base64
 
--- Lib/ldif.py
+++ Lib/ldif.py
@@ -9,7 +9,7 @@ Python compability note:
 Tested with Python 2.0+, but should work with Python 1.5.2+.
 """
 
-__version__ = '2.4.1'
+__version__ = '2.4.2'
 
 __all__ = [
   # constants
