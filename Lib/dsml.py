43517c88a390cd870e172ac320a78216cf8d5219
stroeder <stroeder>
2011-11-25 12:28:50 +0000

    Set release number to 2.4.5

--- Lib/dsml.py
+++ Lib/dsml.py
@@ -10,7 +10,7 @@ Python compability note:
 Tested with Python 2.0+.
 """
 
-__version__ = '2.4.4'
+__version__ = '2.4.5'
 
 import string,base64
 
--- Lib/ldap/__init__.py
+++ Lib/ldap/__init__.py
@@ -8,7 +8,7 @@ $Id$
 
 # This is also the overall release version number
 
-__version__ = '2.4.4'
+__version__ = '2.4.5'
 
 import sys
 
