b60b397896b4b54931f638b8ada470cdef8eb71b
stroeder <stroeder>
2013-06-22 18:35:30 +0000

    Started 2.4.13

--- Lib/dsml.py
+++ Lib/dsml.py
@@ -10,7 +10,7 @@ Python compability note:
 Tested with Python 2.0+.
 """
 
-__version__ = '2.4.12'
+__version__ = '2.4.13'
 
 import string,base64
 
--- Lib/ldap/__init__.py
+++ Lib/ldap/__init__.py
@@ -8,7 +8,7 @@ $Id$
 
 # This is also the overall release version number
 
-__version__ = '2.4.12'
+__version__ = '2.4.13'
 
 import sys
 
