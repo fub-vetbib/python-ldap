dace8448d37e699b3fa27738461b0d5dcc4c84c4
stroeder <stroeder>
2013-11-23 14:09:59 +0000

    More log data in LDAPLock.acquire() and LDAPLock.release()

--- Lib/ldap/__init__.py
+++ Lib/ldap/__init__.py
@@ -68,14 +68,14 @@ class LDAPLock:
     if __debug__:
       global _trace_level
       if _trace_level>=self._min_trace_level:
-        _trace_file.write('***%s %s.acquire()\n' % (self._desc,self.__class__.__name__))
+        _trace_file.write('***%s.acquire() %s %s\n' % (self.__class__.__name__,repr(self),self._desc))
     return self._lock.acquire()
 
   def release(self):
     if __debug__:
       global _trace_level
       if _trace_level>=self._min_trace_level:
-        _trace_file.write('***%s %s.release()\n' % (self._desc,self.__class__.__name__))
+        _trace_file.write('***%s.release() %s %s\n' % (self.__class__.__name__,repr(self),self._desc))
     return self._lock.release()
 
 
